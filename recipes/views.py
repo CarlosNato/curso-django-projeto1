8122243e69473c552769eaaca61c7d35aaa711f6
Carlos dos Santos <carlos.nato@gmail.com>
2022-10-17 19:30:56 -0300

    initial

--- recipes/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-# Create your views here.
-def home(request):
-    return render(request, 'recipes/hpme.html', context={
-        'name': 'Carlos Renato',
-    })
